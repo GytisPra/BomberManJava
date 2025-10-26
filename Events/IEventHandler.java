42075ac53b1ff8084e53def9cdc032151ec75ecc
GytisPra <gpgytis@gmail.com>
2025-10-26 20:26:19 +0200

    deleted events. will be remade later

--- Events/IEventHandler.java
+++
@@ -1,7 +0,0 @@
-package Events;
-
-import java.util.concurrent.CompletableFuture;
-
-public interface IEventHandler<T extends IGameEvent> {
-    CompletableFuture<Void> handleAsync(T gameEvent);
-}
--- Events/IEventPublisher.java
+++
@@ -1,8 +0,0 @@
-package Events;
-
-import java.util.concurrent.CompletableFuture;
-
-public interface IEventPublisher {
-    <T extends IGameEvent> CompletableFuture<Void> publishAsync(T gameEvent);
-    <T extends IGameEvent> void subscribe(IEventHandler<T> handler);
-}
